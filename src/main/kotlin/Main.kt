be4106d58ec219d683a7a78bbf886857e7d13271
Alphonce08 <alphoncerotich30@gmail.com>
2023-04-19 16:53:31 +0000

    introduction to variable kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    var name = "Okello" //string
+    var age = 23 //Int
+    println(name)
+    println(age)
+
+    var sum1 = 145
+    var sum2 =90
+    var res = sum1 + sum2
+    println(res)
+
+    var sub1 = 65
+    var sub3 = 15
+    var r = sub1 - sub3
+    println(r)
+
+    var c = 54
+    var d =33
+    var k = c * d
+    println(k)
+
+    var a = 454
+    var b = 44
+    var p = a / b
+    println(p)
+}
