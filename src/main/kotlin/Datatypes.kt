a5fa5560c05aabdceabd91581504045fa570a27c
Alphonce08 <alphoncerotich30@gmail.com>
2023-04-20 14:58:24 +0000

    introduction to kotlin

---
+++ src/main/kotlin/Datatypes.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var city:String = "Nairobi" //
+    var age:Int = 20 //
+    var domain = 'k' //Char
+    var status:Boolean = true//Boolean
+    var temperature:Float = 35.6F
+    var results:Double = 23.3773
+}
