23e46a27fec1da0b76723fd98bde4ecb94494b4a
Alphonce08 <alphoncerotich30@gmail.com>
2023-04-25 16:53:47 +0000

    functions and when statements

---
+++ src/main/kotlin/WhenStatementEx.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    println("Enter the input")
+    var userInput = readln()
+
+    when  (userInput){
+        "*544#" -> {
+            println("Dear, customer ")
+            println("please select an option ")
+            println("1: subscribe to daily credit  ")
+            println("2: Buy amazing bundle for monthly ")
+            println("3: Buy Dabo Dabo credo ")
+        }
+    }
+
+}
